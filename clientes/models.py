cc183145c7b64b1a25ba77c9e1baf69aeba69133
Tim Morgenstern <tim@infisc.com.br>
2025-06-25 20:43:37 -0300

    model changes

--- clientes/models.py
+++ clientes/models.py
@@ -5,11 +5,25 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=150)
     data_nascimento = models.DateField()
-    cpf = models.CharField(max_length=14, unique=True)
+    cpf = models.CharField(max_length=11, unique=True)
     email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.nome
+    def save(self, *args, **kwargs):
+        if self.nome:
+            self.nome = self.formatar_nome(self.nome)
+        super().save(*args, **kwargs)
+
+    def formatar_nome(self, nome):
+        partes = nome.lower().split()
+        minusculas = ['da', 'de', 'do', 'das', 'dos', 'e',
+                      'em', 'a', 'o', 'as', 'os', 'para', 'com']
+
+        return ' '.join([
+            p if p in minusculas else p.capitalize()
+            for p in partes
+        ])
 
     class Meta:
         verbose_name = "Cliente"
